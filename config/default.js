fe6d1a6f3ff3ee6b640c8113ba1bbad8b64fe56c
dinusha_dck <dinusha@duosoftware.com>
2017-06-27 17:31:22 +0530

    add functionality get csat details based on agent list add csat download csv

--- config/default.js
+++ config/default.js
@@ -3,11 +3,21 @@ module.exports = {
 
     "Mongo":
     {
-        "ip":"45.55.142.207",
+        "ip":"104.236.231.11",
         "port":"27017",
         "dbname":"dvpdb",
         "password":"DuoS123",
-        "user":"duo"
+        "user":"duo",
+        "replicaset" :"104.236.231.11"
+    },
+
+    "Services":
+    {
+
+        "fileServiceHost": "fileservice.app1.veery.cloud",
+        "fileServicePort": 5645,
+        "fileServiceVersion":"1.0.0.0"
+
     },
 
 
@@ -42,5 +52,6 @@ module.exports = {
         "ip": "localhost",
         "port": "4445"
 
-    }
+    },
+    "Token":"eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJzdWtpdGhhIiwianRpIjoiYWEzOGRmZWYtNDFhOC00MWUyLTgwMzktOTJjZTY0YjM4ZDFmIiwic3ViIjoiNTZhOWU3NTlmYjA3MTkwN2EwMDAwMDAxMjVkOWU4MGI1YzdjNGY5ODQ2NmY5MjExNzk2ZWJmNDMiLCJleHAiOjE5MDIzODExMTgsInRlbmFudCI6LTEsImNvbXBhbnkiOi0xLCJzY29wZSI6W3sicmVzb3VyY2UiOiJhbGwiLCJhY3Rpb25zIjoiYWxsIn1dLCJpYXQiOjE0NzAzODExMTh9.Gmlu00Uj66Fzts-w6qEwNUz46XYGzE8wHUhAJOFtiRo"
 };
