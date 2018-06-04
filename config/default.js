db81c03bd0276a5f0e11c4f830bcbef4c4b5709c
sine19 <sineth17@gmail.com>
2018-06-04 10:45:50 +0530

    clear all values
    all default.js values are cleared due to security reasons.

--- config/default.js
+++ config/default.js
@@ -3,18 +3,18 @@ module.exports = {
 
     "Mongo":
     {
-        "ip":"104.236.231.11",
+        "ip":"",
         "port":"27017",
-        "dbname":"dvpdb",
-        "password":"DuoS123",
-        "user":"duo",
-        "replicaset" :"104.236.231.11"
+        "dbname":"",
+        "password":"",
+        "user":"",
+        "replicaset" :""
     },
 
     "Services":
     {
 
-        "fileServiceHost": "fileservice.app1.veery.cloud",
+        "fileServiceHost": "",
         "fileServicePort": 5645,
         "fileServiceVersion":"1.0.0.0"
 
@@ -23,12 +23,12 @@ module.exports = {
   "Redis":
   {
     "mode":"sentinel",//instance, cluster, sentinel
-    "ip": "45.55.142.207",
+    "ip": "",
     "port": 6389,
-    "user": "duo",
-    "password": "DuoS123",
+    "user": "",
+    "password": "",
     "sentinels":{
-      "hosts": "138.197.90.92,45.55.205.92,138.197.90.92",
+      "hosts": "",
       "port":16389,
       "name":"redis-cluster"
     }
@@ -38,13 +38,13 @@ module.exports = {
   "Security":
   {
 
-    "ip" : "45.55.142.207",
+    "ip" : "",
     "port": 6389,
-    "user": "duo",
-    "password": "DuoS123",
+    "user": "",
+    "password": "",
     "mode":"sentinel",//instance, cluster, sentinel
     "sentinels":{
-      "hosts": "138.197.90.92,45.55.205.92,138.197.90.92",
+      "hosts": "",
       "port":16389,
       "name":"redis-cluster"
     }
@@ -64,5 +64,5 @@ module.exports = {
         "port": "4445"
 
     },
-    "Token":"eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJzdWtpdGhhIiwianRpIjoiYWEzOGRmZWYtNDFhOC00MWUyLTgwMzktOTJjZTY0YjM4ZDFmIiwic3ViIjoiNTZhOWU3NTlmYjA3MTkwN2EwMDAwMDAxMjVkOWU4MGI1YzdjNGY5ODQ2NmY5MjExNzk2ZWJmNDMiLCJleHAiOjE5MDIzODExMTgsInRlbmFudCI6LTEsImNvbXBhbnkiOi0xLCJzY29wZSI6W3sicmVzb3VyY2UiOiJhbGwiLCJhY3Rpb25zIjoiYWxsIn1dLCJpYXQiOjE0NzAzODExMTh9.Gmlu00Uj66Fzts-w6qEwNUz46XYGzE8wHUhAJOFtiRo"
+    "Token":""
 };
